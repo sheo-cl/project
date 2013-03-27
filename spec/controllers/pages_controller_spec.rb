a057a62471c1e87b71dc8ab626ccb6b6ad1eed7b
sheo-cl <mlacour@voidmaster.lautre.net>
2013-03-27 18:03:18 +0100

    Pages statiques ok

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,40 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
-    it "should be successful" do
+    it "doit réussir" do
       get 'home'
       response.should be_success
+    end
+    it "doit avoir le bon titre" do
+      get 'home'
+      response.should have_selector("title",:content => "Projet Web - Marie Sheo Coneau-Lacour | Accueil")
     end
   end
 
   describe "GET 'contact'" do
-    it "should be successful" do
+    it "doit réussir" do
       get 'contact'
       response.should be_success
+    end
+    it "doit avoir le bon titre" do
+      get 'contact'
+      response.should have_selector("title",:content => "Projet Web - Marie Sheo Coneau-Lacour | Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "doit réussir" do
+      get 'about'
+      response.should be_success
+    end
+    it "doit avoir le bon titre" do
+      get 'about'
+      response.should have_selector("title",:content => "Projet Web - Marie Sheo Coneau-Lacour | A propos de...")
     end
   end
 
 end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,18 @@
-require 'spork'
-Spork.prefork do
-	ENV["RAILS_ENV"] ||= 'test'
-	require File.expand_path("../../config/environment", __FILE__)
-	require 'rspec/rails'
-	Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-	RSpec.configure do |config|
-	  # uncomment the appropriate line:
-	  # config.mock_with :mocha
-	  # config.mock_with :flexmock
-	  # config.mock_with :rr
-	  config.mock_with :rspec
-	  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-	  config.use_transactional_fixtures = true
-	end
-end
+ENV["RAILS_ENV"] ||= 'test'
 
-Spork.each_run do
+require File.expand_path("../../config/environment", __FILE__)
+
+require 'rspec/rails'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+	# uncomment the appropriate line:
+	# config.mock_with :mocha
+	# config.mock_with :flexmock
+	# config.mock_with :rr
+	config.mock_with :rspec
+	config.fixture_path = "#{::Rails.root}/spec/fixtures"
+	config.use_transactional_fixtures = true
 end
 
