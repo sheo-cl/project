11b3928910ce2a175550168490646faef3d6f500
sheo-cl <mlacour@voidmaster.lautre.net>
2013-03-28 22:19:11 +0100

    Inscription user terminée

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def new
-    @titre = "Inscription"
+    @user = User.new
+    @titre = "S'inscrire !"
   end
   def show
   	# params = objet standard de Rails pour récupérer l'identifiant :
     @user = User.find(params[:id])
     @titre = @user.nom
   end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Inscription réussie !"
+      redirect_to @user
+    else
+      @titre = "S'inscrire !"
+      render 'new'
+    end
+  end
 end
+
